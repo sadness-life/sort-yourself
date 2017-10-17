ea3743ea0b805b6950f14b73b9733f611bec3b4c
Nandira Paturohman <nandirasp@gmail.com>
2017-10-17 20:17:01 +0700

    selection/bubble/insertion sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,22 +1,19 @@
 'use strict'
 
-function bubbleSort(items) {
-   for (var i = items.length-1; i>=0; i--){
-     for(var j = 1; j<=i; j++){
-       if(items[j-1]>items[j]){
-           var temp = items[j-1];
-           items[j-1] = items[j];
-           items[j] = temp;
-        }
-     }
-   }
-   return items;
+function insertionSort(arrays){
+  for (var i = 1; i < arrays.length; i++) {
+    for (var j = 0; j < i; j++) {
+      if(arrays[i] < arrays[j]){
+        let temp = arrays[i]
+        arrays[i] = arrays[j]
+        arrays[j] = temp
+      }
+    }
+  }
+  return arrays
 }
-
-
 // Driver code
 function main() {
-
   sort_from_file('test_sortwordlist.txt')
   // sort_from_file('random_wordlist.txt')
   // sort_from_file('reversed_wordlist.txt')
@@ -32,12 +29,11 @@ function sort_from_file(filename) {
     if (err)
       return console.log(err)
 
-    let items = data.toString()
-      .split("\n")
+    let items = data.toString().split('\n')
 
     console.log(filename)
     console.log("--------")
-    console.log(bubbleSort(items))
+    console.log(insertionSort(items))
 
   })
 }
--- selection_sort.js
+++ selection_sort.js
@@ -1,14 +1,27 @@
 'use strict'
 
 function selectionSort(arrays){
+  for (var i = 0; i < arrays.length - 1; i++) {
+    var min = i;
+    for (var j = i + 1; j < arrays.length; j++) {
+      if (arrays[j] < arrays[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      var temp = arrays[i];
+      arrays[i] = arrays[min];
+      arrays[min] = temp;
+    }
+  }
+  return arrays
 }
-
 // Driver code
 function main() {
-
-  sort_from_file('random_wordlist.txt')
-  sort_from_file('reversed_wordlist.txt')
-  sort_from_file('sorted_wordlist.txt')
+  sort_from_file('test_sortwordlist.txt')
+  // sort_from_file('random_wordlist.txt')
+  // sort_from_file('reversed_wordlist.txt')
+  // sort_from_file('sorted_wordlist.txt')
 
 }
 
@@ -20,7 +33,7 @@ function sort_from_file(filename) {
     if (err)
       return console.log(err)
 
-    let items = data.toString().split("\r\n")
+    let items = data.toString().split('\n')
 
     console.log(filename)
     console.log("--------")
